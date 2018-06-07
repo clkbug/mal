faa2872d6c740300518c81aa8c2ebe74684e1e44
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2018-06-07 21:59:54 +0900

    add go2's step3_env

---
+++ go2/src/step3_env/env.go
@@ -0,0 +1,83 @@
+package main
+
+// Env : Environment
+type Env map[Symbol]SExp
+
+func (e Env) set(sym Symbol, sexp SExp) {
+	e[sym] = sexp
+}
+
+func (e Env) get(sym Symbol) (SExp, bool) {
+	v, ok := e[sym]
+	return v, ok
+}
+
+var replEnv Env
+
+func initREPLEnv() {
+	replEnv = make(map[Symbol]SExp)
+	plus := Closure{
+		env: nil,
+		fun: Func(func(_ Env, args List) SExp {
+			s := 0
+			for _, v := range args {
+				switch v.(type) {
+				case Int:
+					s += int(v.(Int))
+				default:
+					panic("+ expects int, but got ...")
+				}
+			}
+			return Int(s)
+		}),
+	}
+	minus := Closure{
+		env: nil,
+		fun: Func(func(_ Env, args List) SExp {
+			s := int(args[0].(Int))
+			for _, v := range args[1:] {
+				switch v.(type) {
+				case Int:
+					s -= int(v.(Int))
+				default:
+					panic("- expects int, but got ...")
+				}
+			}
+			return Int(s)
+		}),
+	}
+	times := Closure{
+		env: nil,
+		fun: Func(func(_ Env, args List) SExp {
+			s := 1
+			for _, v := range args {
+				switch v.(type) {
+				case Int:
+					s *= int(v.(Int))
+				default:
+					panic("* expects int, but got ...")
+				}
+			}
+			return Int(s)
+		}),
+	}
+	div := Closure{
+		env: nil,
+		fun: Func(func(_ Env, args List) SExp {
+			s := int(args[0].(Int))
+			for _, v := range args[1:] {
+				switch v.(type) {
+				case Int:
+					s /= int(v.(Int))
+				default:
+					panic("/ expects int, but got ...")
+				}
+			}
+			return Int(s)
+		}),
+	}
+	replEnv.set(Symbol("+"), plus)
+	replEnv.set(Symbol("-"), minus)
+	replEnv.set(Symbol("*"), times)
+	replEnv.set(Symbol("/"), div)
+}
---
+++ go2/src/step3_env/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func read(scanner *bufio.Scanner) (SExp, error) {
+	s := scanner.Text()
+	if len(s) == 0 {
+		os.Exit(0)
+	}
+	r := initReader(s)
+	return r.readForm()
+}
+
+func eval(e SExp) SExp { return e.eval(replEnv) }
+
+func print(e SExp) {
+	fmt.Println(e.toString())
+}
+
+func init() {
+	initREPLEnv()
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("user> ")
+	for scanner.Scan() {
+		s, err := read(scanner)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			print(eval(s))
+		}
+		fmt.Print("user> ")
+	}
+}
