147cd75c123d3bdd0edd26e884807f82b9a181fa
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2018-06-16 22:09:25 +0900

    add step5_tco files

---
+++ go2/src/step5_tco/env.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+type envInternal map[Symbol]SExp
+
+func makeEnvInternal() envInternal { return make(envInternal) }
+
+// Env : Environment
+type Env struct {
+	env     envInternal
+	nextEnv *Env
+}
+
+func (e Env) set(sym Symbol, sexp SExp) {
+	e.env[sym] = sexp
+}
+
+func (e Env) get(sym Symbol) (SExp, bool) {
+	v, ok := e.env[sym]
+	if ok || e.nextEnv == nil {
+		return v, ok
+	}
+	return e.nextEnv.get(sym)
+}
+
+func (e Env) del(sym Symbol) {
+	delete(e.env, sym)
+}
+
+func makeNewEnv(e Env) Env {
+	return Env{
+		env:     make(envInternal),
+		nextEnv: &e,
+	}
+}
+
+func (e Env) copy() Env {
+	var ne Env
+	if e.nextEnv == nil {
+		ne.env = make(envInternal)
+	} else {
+		ne = e.nextEnv.copy()
+	}
+	for k, v := range e.env {
+		ne.env[k] = v.copy()
+	}
+	return ne
+}
+
+var replEnv Env
+
+func init() {
+	replEnv = Env{
+		env:     makeEnvInternal(),
+		nextEnv: nil,
+	}
+	plus := CoreFunc(
+		func(args List) (SExp, error) {
+			s := 0
+			for _, v := range args {
+				switch v.(type) {
+				case Int:
+					s += int(v.(Int))
+				default:
+					return UNDEF, errors.New("invalid +'s argument")
+				}
+			}
+			return Int(s), nil
+		})
+	minus := CoreFunc(
+		func(args List) (SExp, error) {
+			s := int(args[0].(Int))
+			for _, v := range args[1:] {
+				switch v.(type) {
+				case Int:
+					s -= int(v.(Int))
+				default:
+					return UNDEF, errors.New("invalid -'s argument")
+				}
+			}
+			return Int(s), nil
+		})
+	times := CoreFunc(
+		func(args List) (SExp, error) {
+			s := 1
+			for _, v := range args {
+				switch v.(type) {
+				case Int:
+					s *= int(v.(Int))
+				default:
+					return UNDEF, errors.New("invalid *'s argument")
+				}
+			}
+			return Int(s), nil
+		})
+	div := CoreFunc(
+		func(args List) (SExp, error) {
+			s := int(args[0].(Int))
+			for _, v := range args[1:] {
+				switch v.(type) {
+				case Int:
+					s /= int(v.(Int))
+				default:
+					return UNDEF, errors.New("invalid +'s argument")
+				}
+			}
+			return Int(s), nil
+		})
+	cmp := func(f func(x, y int) bool) CoreFunc {
+		return CoreFunc(func(args List) (SExp, error) {
+			if len(args) < 2 {
+				return UNDEF, errors.New("few arguments for <,<=,>,>=")
+			}
+			switch x := args[0].(type) {
+			case Int:
+				switch y := args[1].(type) {
+				case Int:
+					return Bool(f(int(x), int(y))), nil
+				}
+			}
+			return UNDEF, errors.New("arguments for '<' should be Int")
+		})
+	}
+	lt := cmp(func(x, y int) bool { return x < y })
+	le := cmp(func(x, y int) bool { return x <= y })
+	gt := cmp(func(x, y int) bool { return x > y })
+	ge := cmp(func(x, y int) bool { return x >= y })
+	eq := CoreFunc(func(args List) (SExp, error) {
+		if len(args) < 2 {
+			return UNDEF, errors.New("few arguments for =")
+		}
+		return Bool(args[0].isSame(args[1])), nil
+	})
+	list := CoreFunc(
+		func(args List) (SExp, error) {
+			return args, nil
+		})
+	listq := CoreFunc(
+		func(args List) (SExp, error) {
+			switch args[0].(type) {
+			case List:
+				return Bool(true), nil
+			default:
+				return Bool(false), nil
+			}
+		})
+	emptyq := CoreFunc(
+		func(args List) (SExp, error) {
+			switch args[0].(type) {
+			case List:
+				return Bool(len(args[0].(List)) == 0), nil
+			case Vector:
+				return Bool(len(args[0].(Vector)) == 0), nil
+			default:
+				return Bool(false), nil
+			}
+		})
+	count := CoreFunc(
+		func(args List) (SExp, error) {
+			switch args[0].(type) {
+			case List:
+				return Int(len(args[0].(List))), nil
+			case Vector:
+				return Int(len(args[0].(Vector))), nil
+			default:
+				return Int(0), nil
+			}
+		})
+	not := CoreFunc(func(args List) (SExp, error) {
+		b := true
+		switch args[0].(type) {
+		case NilType:
+			b = false
+		case Bool:
+			b = bool(args[0].(Bool))
+		case List:
+			list := args[0].(List)
+			b = len(list) == 0
+		}
+		return Bool(!b), nil
+	})
+	do := CoreFunc(
+		func(args List) (SExp, error) {
+			return args[len(args)-1], nil
+		})
+	prstr := CoreFunc(
+		func(args List) (SExp, error) {
+			s := printStrList(args, true, " ")
+			return StringLiteral(s), nil
+		})
+	prn := CoreFunc(
+		func(args List) (SExp, error) {
+			println(printStrList(args, true, " "))
+			return NIL, nil
+		})
+	str := CoreFunc(
+		func(args List) (SExp, error) {
+			s := ""
+			for _, a := range args {
+				s += a.printStr(false)
+			}
+			return StringLiteral(s), nil
+		})
+	printlnCF := CoreFunc(
+		func(args List) (SExp, error) {
+			println(printStrList(args, false, " "))
+			return NIL, nil
+		})
+	replEnv.set(Symbol("+"), plus)
+	replEnv.set(Symbol("-"), minus)
+	replEnv.set(Symbol("*"), times)
+	replEnv.set(Symbol("/"), div)
+	replEnv.set(Symbol("<"), lt)
+	replEnv.set(Symbol("<="), le)
+	replEnv.set(Symbol(">"), gt)
+	replEnv.set(Symbol(">="), ge)
+	replEnv.set(Symbol("="), eq)
+	replEnv.set(Symbol("list"), list)
+	replEnv.set(Symbol("list?"), listq)
+	replEnv.set(Symbol("empty?"), emptyq)
+	replEnv.set(Symbol("count"), count)
+	replEnv.set(Symbol("not"), not)
+	replEnv.set(Symbol("do"), do)
+	replEnv.set(Symbol("prn"), prn)
+	replEnv.set(Symbol("str"), str)
+	replEnv.set(Symbol("pr-str"), prstr)
+	replEnv.set(Symbol("println"), printlnCF)
+}
+
+func printStrList(sexps List, isReadable bool, sep string) string {
+	s := make([]string, len(sexps))
+	for i, e := range sexps {
+		s[i] = e.printStr(isReadable)
+	}
+	return strings.Join(s, sep)
+}
---
+++ go2/src/step5_tco/specialforms.go
@@ -0,0 +1,151 @@
+package main
+
+import "errors"
+
+const IF = "if"
+const COND = "cond"
+const OR = "or"
+const DEF = "def!"
+const DEFMACRO = "defmacro!"
+const LET = "let*"
+const FN = "fn*"
+
+var specialFormMap = map[string]struct{}{
+	IF: struct{}{}, COND: struct{}{}, OR: struct{}{},
+	DEF: struct{}{}, DEFMACRO: struct{}{}, LET: struct{}{},
+	FN: struct{}{},
+}
+
+func isSpecialForm(s SExp) (string, bool) {
+	switch s.(type) {
+	case Symbol:
+		_, ok := specialFormMap[string(s.(Symbol))]
+		return string(s.(Symbol)), ok
+	default:
+		return "", false
+	}
+}
+
+func evalIf(env Env, l List) (SExp, error) {
+	cond := true
+	c, err := l[0].eval(env)
+	if err != nil {
+		return UNDEF, err
+	}
+	switch c := c.(type) {
+	case NilType:
+		cond = false
+	case Bool:
+		cond = bool(c)
+	}
+	if cond {
+		return l[1].eval(env)
+	} else if len(l) >= 3 {
+		return l[2].eval(env)
+	} else {
+		return NIL, nil
+	}
+}
+
+func evalDef(env Env, l List) (SExp, error) {
+	switch l[0].(type) {
+	case Symbol:
+		s := l[0].(Symbol)
+		v, err := l[1].eval(env)
+		if err != nil {
+			return UNDEF, err
+		}
+		env.set(s, v)
+
+		// if def! defines a recursive function,...
+		switch l := l[1].(type) {
+		case List:
+			switch t := l[0].(type) {
+			case Symbol:
+				if t == Symbol(FN) {
+					switch v := v.(type) {
+					case Closure:
+						v.env.set(s, v)
+						v.name = s
+					default:
+						panic("can't reach here")
+					}
+				}
+			}
+		}
+		return v, nil
+	default:
+		return UNDEF, errors.New("'(def! SYMBOL EXP)'")
+	}
+}
+
+func evalLet(env Env, l List) (SExp, error) {
+	switch l[0].(type) {
+	case List:
+		vars := l[0].(List)
+		body := l[1]
+		tmpEnv := makeNewEnv(env)
+		if len(vars)%2 != 0 {
+			return UNDEF, errors.New("Syntax Error: let*'s bind")
+		}
+		for i := 0; i < len(vars); i += 2 {
+			evalLetBindOne(tmpEnv, vars[i:i+2])
+		}
+		return body.eval(tmpEnv)
+
+	case Vector:
+		vars := l[0].(Vector).toList()
+		body := l[1]
+		tmpEnv := makeNewEnv(env)
+		if len(vars)%2 != 0 {
+			return UNDEF, errors.New("Syntax Error: let*'s bind")
+		}
+		for i := 0; i < len(vars); i += 2 {
+			evalLetBindOne(tmpEnv, vars[i:i+2])
+		}
+		return body.eval(tmpEnv)
+	default:
+		return UNDEF, errors.New("Syntax error: let*")
+	}
+}
+
+func evalLetBindOne(env Env, l List) error {
+	switch l[0].(type) {
+	case Symbol:
+		vname := l[0].(Symbol)
+		value, err := l[1].eval(env)
+		if err != nil {
+			return err
+		}
+		env.set(vname, value)
+		return nil
+	default:
+		return errors.New("Syntax error: let*'s bind")
+	}
+}
+
+func evalFn(env Env, l List) (SExp, error) {
+	var params []SExp
+	switch l[0].(type) {
+	case List:
+		params = l[0].(List)
+	case Vector:
+		params = l[0].(Vector)
+	default:
+		return UNDEF, errors.New("unimplemented")
+	}
+	cparams := make([]Symbol, len(params))
+	for i, p := range params {
+		switch p.(type) {
+		case Symbol:
+			cparams[i] = p.(Symbol)
+		default:
+			return UNDEF, errors.New("fn* param should be SYMBOL ... but got " + p.toString())
+		}
+	}
+	return Closure{
+		env:    env.copy(),
+		params: cparams,
+		body:   l[1],
+	}, nil
+}
